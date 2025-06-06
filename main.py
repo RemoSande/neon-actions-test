335d171ee0bb0065264204e55f1f4faf9cf67101
artdojo // aidojo <77502320+RemoSande@users.noreply.github.com>
2025-06-06 13:54:53 +0200

    feat: added mock porting_setup() function in main

--- main.py
+++ main.py
@@ -2,5 +2,11 @@ def main():
     print("Hello from github-actions-python-fastapi!")
 
 
+def porting_setup():
+    """Notification that porting is being set-up"""
+    print("Hello there, the ports are being routed as we speak, stand-by: ")
+
+
+
 if __name__ == "__main__":
     main()
